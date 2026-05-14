b3c43f9b37ad8ab2bd213878505346c93b68775c
Bryam Perez <bryamperez0710@gmail.com>
2026-05-14 19:28:41 -0400

    refactor(icons): create and configure index exports for icons

---
+++ src/assets/icons/index.js
@@ -0,0 +1,48 @@
+//Profile
+import perfil from "./perfil.jpg";
+
+//Logos Skills
+import css from "./css_logo.svg";
+import html from "./html_logo.svg";
+import javaScript from "./javascript_logo.svg";
+import githubLogo from "./github_logo.svg";
+import git from "./git.svg";
+import tailwind from "./tailwind_logo.svg";
+import react from "./react_logo.svg";
+
+//Experience
+import maletinCyan from "./maletin_cyan.svg";
+import maletinAzulOscuro from "./maletin_azulOscuro.svg";
+
+//Contact
+import mailCyan from "./mail_cyan.svg";
+import githubGray from "./github_gray.svg";
+import linkedinGray from "./linkedin_gray.svg";
+
+//Projects
+import githubCyan from "./github_cyan.svg";
+
+
+export const Icons = {
+    logos: {
+        html,
+        css,
+        javaScript,
+        githubLogo,
+        git,
+        tailwind,
+        react
+    },
+    contact: {
+        mailCyan,
+        githubGray,
+        linkedinGray
+    },
+    experience: {
+        maletinCyan,
+        maletinAzulOscuro
+    },
+    projects: {
+        githubCyan
+    }
+};
